833d87150da171509892b09cf92ca7ff116d5a15
imran <zunaeede@gmail.com>
2023-12-06 15:30:13 +0600

    practice - practice project done

--- 1_practice.php
+++ 1_practice.php
@@ -1,10 +1,14 @@
 <?php
 
-class Opp{
-  public $x;
-  function y(){
-    
-  }
+class test_1{
+  public $test_1_1;
+    function test1_2(){
+        echo "this is test" . $this->test_1_1;
+    }
 }
 
+$test_1_3 = new test_1();
+$test_1_3->test_1_1 = "Project_1";
+
+echo $test_1_3->test1_2();
 ?>
