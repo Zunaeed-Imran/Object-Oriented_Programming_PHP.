4e653206212b528a87870d4dae08d6a91bf44671
imran <zunaeede@gmail.com>
2023-12-06 16:52:54 +0600

    practice - 2nd test project

--- practice_2.php
+++ practice_2.php
@@ -1,4 +1,13 @@
 <?php
 
+class test_1_class {
+  public $test_2_public;
+  function test_2_function(){
+    echo "This is " . $this->test_2_public;
+  }
+}
+$test_2 = new test_1_class();
+$test_2->test_2_public = "2nd Test Project";
 
+echo $test_2->test_2_function();
 ?>
