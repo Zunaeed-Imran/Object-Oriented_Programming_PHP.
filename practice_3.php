9296644345b91db3a27a48a78c03429b2874656f
imran <zunaeede@gmail.com>
2023-12-06 18:00:34 +0600

    practice - practicing 3rd time.

--- practice_3.php
+++ practice_3.php
@@ -1,10 +1,14 @@
 <?php
 class test_3{
-  public $a, $b, $c;
-  function sum(){
-    
+  public $a;
+  function test_fun(){
+    $this->a = "King-Arther";
+    return $this->a;
   }
 }
 
+$b = new test_3();
 
+
+echo $b->a;
 ?>
