333e3f994200ece3e76c2d64ccb98479b21255a4
imran <zunaeede@gmail.com>
2024-07-03 18:39:44 +0600

    main - concat and add some command to understand better.

--- index.php
+++ index.php
@@ -1,6 +1,12 @@
 <?php
+
+// simple and Basic OOP.
+// we don't use setter getter function.
+// also don't use return.
 class person{
+
   public $name;
+
   function show(){
     echo "Your name is : ". $this->name;
   }
