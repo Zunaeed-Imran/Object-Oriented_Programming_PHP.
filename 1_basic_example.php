5cfa427450655b358e8f4f76f4e645a8b8a39cda
imran <zunaeede@gmail.com>
2024-07-03 17:06:16 +0600

    main - add a example of OPP basic practice.

---
+++ 1_basic_example.php
@@ -0,0 +1,35 @@
+<?php
+// basic example of class or OOP.
+
+class Fruits{
+
+  public $name;
+  public $color;
+
+
+  function set_name($name){
+    $this->name = $name;
+  }
+
+  function get_name(){
+    return $this->name;
+  }
+
+}
+
+$apple = new Fruits();
+$banana = new Fruits();
+$orange = new Fruits();
+
+$apple->set_name('Apple');
+$banana->set_name('Banana');
+$orange->set_name('Orange');
+
+echo $apple->get_name();
+echo "</br>";
+echo $banana->get_name();
+echo "</br>";
+echo $orange->get_name();
+
+
+?>
