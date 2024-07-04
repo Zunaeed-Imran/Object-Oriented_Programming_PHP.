738730b1c9c27a07c7256220ce9e25d37d7fb07c
imran <zunaeede@gmail.com>
2024-07-04 11:16:43 +0600

    main - simple constructor practice.

--- Constructor_OOP/index.php
+++ Constructor_OOP/index.php
@@ -1,10 +1,22 @@
 <?php
 
-//  in this file we practice simple constructor.
+//  in this file we practice simple constructor OPP in PHP.
 
+class Fruits{
 
+  public $name, $color;
 
+  function __construct($name){
+    $this->name = $name;
+  }
 
+  function get_name(){
+    return $this->name;
+  }
+}
+
+$apple = new Fruits("Apple Fruits");
+echo $apple->get_name();
 
 
 
