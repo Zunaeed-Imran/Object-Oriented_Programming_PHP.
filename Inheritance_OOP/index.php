2fdcb9a400540d79640a15a7b5f90285550ad59e
imran <zunaeede@gmail.com>
2024-07-04 16:28:58 +0600

    main - practice extends class or inharitance.

--- Inheritance_OOP/index.php
+++ Inheritance_OOP/index.php
@@ -4,7 +4,32 @@
 
 
 
+// the father class
+class Fruits{
 
+  public $name, $color;
 
+  public function __construct($name, $color){
+    $this->name = $name;
+    $this->color = $color;
+  }
+
+  public function intro(){
+    echo "The Fruits is {$this->name}, and the color is {$this->color}";
+  }
+}
+
+// take another class to inharit (উত্তরাধিকারী)
+
+class Strawberry extends Fruits{
+  public function message(){
+    echo "Fruit or Berry \n";
+  }
+}
+
+$berry = new Strawberry("Stwarberry", "Red");
+
+$berry->message();
+$berry->intro();
 
 ?>
