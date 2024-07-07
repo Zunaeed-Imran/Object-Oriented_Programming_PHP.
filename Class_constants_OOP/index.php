73589728bea9b08dba73720224151fd7a9d19233
imran <zunaeede@gmail.com>
2024-07-07 09:54:31 +0600

    main - add a practice file for php class constants.

---
+++ Class_constants_OOP/index.php
@@ -0,0 +1,12 @@
+<?php
+// Practicing PHP Class Constants
+// We can access a constant from outside the class 
+
+class Hello{
+  const Goodbye = "Test Message for PHP Class Constant in OOP";
+}
+
+echo Hello::Goodbye;
+
+
+?>
