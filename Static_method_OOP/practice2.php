5f66d394e7846592fd62b8feb3e36230bfb0e085
imran <zunaeede@gmail.com>
2024-07-08 11:44:19 +0600

    main - another practice of static method.

---
+++ Static_method_OOP/practice2.php
@@ -0,0 +1,13 @@
+<?php
+// another way of output static method.
+
+class Name2{
+  public static function nameCall2(){
+    echo "Hello New Programmer How Are You";
+  }
+}
+
+// another way to call it directly.
+Name2::nameCall2();
+
+?>
