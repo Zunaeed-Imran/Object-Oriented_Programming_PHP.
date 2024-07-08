8b94f9644dce5d85be2c2e52f2cc23a173b45b29
imran <zunaeede@gmail.com>
2024-07-08 09:46:14 +0600

    main - add anotehr importants command.

--- Interfaces_OOP_PHP/index.php
+++ Interfaces_OOP_PHP/index.php
@@ -1,6 +1,7 @@
 <?php
 // interfaces practice in PHP
 // Interfaces make it easy to use a variety of different classes in the same way.
+//  All interface methods must be public
 
 interface Animal {
   public function talk();
